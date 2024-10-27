699997a49da8c52220567afa430423bb7630df1c
Priyanshu Singh <sbhola833@gmail.com>
2024-10-27 22:37:28 +0530

    add registeration and login

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
+from .forms import UserRegisterForm
 
 # Create your views here.
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)  # Get the user object without saving to the database
+            user.first_name = form.cleaned_data.get('first_name')
+            user.last_name = form.cleaned_data.get('last_name')
+            user.save()  # Now save the user to the database
+            messages.success(request, "Registration successful. You can now log in.")
+            return redirect('user_login')
+        else:
+            messages.error(request, "Registration failed. Please correct the errors below.")
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'authentication/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('home')
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request,("there is an error occurd retry plz!"))
+            return redirect('user_login')
+                   
+    return render(request,'authentication/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request,("Logout  was sucessfull"))
+    return redirect(reverse('user_login'))
+
+
--- core/urls.py
+++ core/urls.py
@@ -21,8 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('',include("apps.home.urls")),
-
+    path('',include("apps.home.urls")),
+    path('user/',include("apps.authentication.urls")),
+    path('user/',include("django.contrib.auth.urls")),
+    
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
